8debc7533879ff253d3c928cd0ae00da609a345d
EconClass <bizmail327@gmail.com>
2018-09-21 16:48:29 -0700

    Delete function working

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -5,13 +5,14 @@ window.onload = function () {
         // prevent the default form behavior
         e.preventDefault();
 
+        // serialize the form data into an object
         let comment = $(newCommentForm).serialize();
 
         // use axios to initialize a post request and send in the form data
         axios.post(`/movies/${comment.movieId}/reviews/comments`, comment)
         .then(function (response) {
 
-            // we get the comment on a JSON format from the response
+            // Comment is in a JSON format from the response
             let newComment = response.data.comment;
             newCommentForm.reset();
 
@@ -19,9 +20,10 @@ window.onload = function () {
                 `
                 <div class="card">
                     <div class="card-block">
+                        <h4 class="card-title">${newComment.title}</h4>
                         <p class="card-text">${newComment.content}</p>
                         <p>
-                            <form method="POST" action="/reviews/comments/${newComment._id}?_method=DELETE">
+                            <form method="POST" action="/movies/{{review.movieId}}/reviews/comments/${newComment._id}?_method=DELETE">
                                 <button class="btn btn-link" type="submit">Delete</button>
                             </form>
                         </p>
@@ -35,57 +37,20 @@ window.onload = function () {
             alert('There was a problem saving your comment. Please try again.')
         });
     });
-        // // serialize the form data into an object
-        // let comment = {};
-        // const inputs = document.getElementsByClassName('form-control');
-        // for (var i = 0; i < inputs.length; i++) {
-        //     comment[inputs[i].name] = inputs[i].value;
-        // };
-        //
-        // axios.post('/reviews/comments', comment)
-        // .then(function (response) {
-        //     // wait for the success response from the server
-        //     console.log(response);
-        //     // remove the information from the form
-        //     // display the data as a new comment on the page
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        //     // handle any errors
-        //     alert('There was a problem saving your comment. Please try again.')
-        // });
-        //
-        // axios.post('/user', comment)
-        // .then(function (response) {
-        //     // wait for the success response from the server
-        //     console.log(response);
-        //     // remove the information from the form
-        //     this.reset();
-        //     // display the data as a new comment on the page
-        //     document.getElementById('comments').prepend(
-        //         `
-        //         <div class="card">
-        //             <div class="card-block">
-        //                 <h4 class="card-title">${response.title}</h4>
-        //                 <p class="card-text">${response.content}</p>
-        //                 <p>
-        //                     <form method="POST" action="/reviews/comments/${response._id}?_method=DELETE">
-        //                         <button class="btn btn-link" type="submit">Delete</button>
-        //                     </form>
-        //                 </p>
-        //             </div>
-        //         </div>
-        //         `
-        //     );
-        // });
 
-    document.getElementById('delete-comment').addEventListener('click', (e) => {
-        console.log("click!")
-        let commentId = this.getAttribute('data-comment-id')
-        axios.delete(`/reviews/comments/${commentId}`)
+    let d_comment = document.getElementById('deleteComment');
+
+    document.getElementById('deleteComment').addEventListener('click', (e) => {
+        console.log("click!");
+        console.log(d_comment);
+        let commentId = d_comment.getAttribute('data-comment-id');
+        console.log(commentId);
+        let delComment = $(d_comment).serialize();
+        axios.delete(`/movies/${delComment.movieId}/reviews/comments/${commentId}`)
         .then(response => {
-            console.log(response)
-            comment = document.getElementById(commentId)
+            let comment = document.getElementById(commentId);
+            console.log(comment);
+            console.log(response);
             comment.parentNode.removeChild(comment); // OR comment.style.display = 'none';
         })
         .catch(error => {
