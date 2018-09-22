2a196336da4af0cf2d0554e7b10de06b863a6a4a
EconClass <bizmail327@gmail.com>
2018-09-21 17:49:31 -0700

    admin added

---
+++ controllers/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const Review = require('../models/review')
+
+// NEW Comment
+app.get('/admin', (req, res) => {
+    Review.find()
+    .then(reviews => {
+        res.render('admin', { reviews: reviews });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+module.exports = app;
